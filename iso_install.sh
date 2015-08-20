3ee4da2bfc4093b61f50b875a6e4031f1a9c0a18
Guangzheng Zhang <zhang.elinks@gmail.com>
2015-08-21 05:44:03 +0800

    add iso_install.sh

--- iso_install.sh
+++ iso_install.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
+set -e 
 
-echo "installing ..."
+show_usage(){
+cat <<EOF
+
+Usage: 	    ${0##*/}  <device>
+
+Example:    ${0##*/}  /dev/sda
+
+EOF
+}
+
+device=$1
+[ -z "${device}" ] && {
+	show_usage
+	exit 1
+}
+
+[ -b "${device}" -a -w "${device}" ] || {
+	echo "ERR: [${device}] must be block writable block device"
+	exit 1
+}
+
+mount -o loop /dev/cdrom /mnt
+bunzip2 -c  /mnt/bzimage/coreos_production_image.bin.bz2 > "${device}"
+blockdev --rereadpt "${device}"
+
+echo "Install to ${device} Finished!"
