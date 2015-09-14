b7d0dfcb5a6019db0c3ddff7f836f486257e5f58
Guangzheng Zhang <zhang.elinks@gmail.com>
2015-09-14 23:52:23 +0800

    emerge install skydns

--- build_library/prod_image_util.sh
+++ build_library/prod_image_util.sh
@@ -49,6 +49,7 @@ create_prod_image() {
   emerge_to_image "${root_fs_dir}" "${base_pkg}"
   emerge_to_image "${root_fs_dir}" "csphere/prometheus"
   emerge_to_image "${root_fs_dir}" "csphere/csphere"
+  emerge_to_image "${root_fs_dir}" "csphere/skydns"
   write_packages "${root_fs_dir}" "${BUILD_DIR}/${image_packages}"
   write_licenses "${root_fs_dir}" "${BUILD_DIR}/${image_licenses}"
   extract_docs "${root_fs_dir}"
