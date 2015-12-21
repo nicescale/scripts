638eb06b4b099f1d87b41444a5bc1bafa1e3de07
Guangzheng Zhang <zhang.elinks@gmail.com>
2015-12-21 13:56:46 +0800

    fix

--- csphere_prepare_kernel.sh
+++ csphere_prepare_kernel.sh
@@ -8,5 +8,7 @@ firmware_url=$2
 
 BINDEST=$(get_bindest)
 mkdir -p "${BINDEST}sys-kernel"
+echo "downloading $kernel_url ..."
 curl -sS $kernel_url > ${BINDEST}${KERNEL_NAME}${SUFFIXTBZ}
+echo "downloading $firmware ..."
 curl -sS $firmware_url > ${BINDEST}${FIRMWARE_NAME}${SUFFIXTBZ}
