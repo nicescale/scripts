5346b8616d5bb8c3c012b9522ea54c3a5a84a6c2
Shijiang Wei <mountkin@gmail.com>
2016-01-19 00:05:24 +0800

    temporarily fix csphere frontend version

--- csphere_rebuild.sh
+++ csphere_rebuild.sh
@@ -3,6 +3,8 @@ set -e
 
 version=$(cat ../third_party/csphere/VERSION.txt)
 SUBFFIX=$(echo ${version:-master}|cut -d. -f1,2)
+# temporary fix version
+SUBFFIX=1.2
 assets_url="http://tsing:e0cab9e41247ec200b7eb6ec5cb159ec@ci.csphe.re/job/csphere-fe/lastSuccessfulBuild/artifact/dist/assets-${SUBFFIX}.tgz"
 
 mongod_url="http://52.68.20.57/cos-files/mongo-3.0.3.tgz"
