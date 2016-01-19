5282badcbf7deef30e533ef8826f2c9246d07e15
Guangzheng Zhang <zhang.elinks@gmail.com>
2016-01-19 23:03:16 +0800

    detect real in use commit of csphere before download assets

--- csphere_rebuild.sh
+++ csphere_rebuild.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 set -e
 
-version=$(cat ../third_party/csphere/VERSION.txt)
+# detect real in use commit
+eval "$(awk '/^[ \t]*CROS_WORKON_COMMIT=/' ../third_party/coreos-overlay/csphere/csphere/csphere-9999.ebuild)"
+if [ "${CROS_WORKON_COMMIT}" != "-" ]; then
+	rm -rf /tmp/.csphere.tmp
+	git clone ../third_party/csphere/ /tmp/.csphere.tmp
+	git -C /tmp/.csphere.tmp checkout ${CROS_WORKON_COMMIT} VERSION.txt
+	version=$(cat /tmp/.csphere.tmp/VERSION.txt)
+else
+	version=$(cat ../third_party/csphere/VERSION.txt)
+fi
+
 SUBFFIX=$(echo ${version:-master}|cut -d. -f1,2)
-# temporary fix version
-SUBFFIX=1.2
 assets_url="http://tsing:e0cab9e41247ec200b7eb6ec5cb159ec@ci.csphe.re/job/csphere-fe/lastSuccessfulBuild/artifact/dist/assets-${SUBFFIX}.tgz"
 
 mongod_url="http://52.68.20.57/cos-files/mongo-3.0.3.tgz"
